3e20637b3ec643046171322e62ec082a9c973989
Hyesung82 <khs990419@gmail.com>
2024-06-30 23:07:24 +0900

    [design] 랭킹 표시 디자인 수정

--- presentation/src/main/java/com/daejol/presentation/home/PopularCatDogCard.kt
+++ presentation/src/main/java/com/daejol/presentation/home/PopularCatDogCard.kt
@@ -1,5 +1,6 @@
 package com.daejol.presentation.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,12 +19,14 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -33,8 +36,9 @@ import coil.request.ImageRequest
 import com.daejol.presentation.R
 import com.daejol.presentation.ui.theme.CatdogcupTheme
 import com.daejol.presentation.ui.theme.MoveSans
+import com.daejol.presentation.ui.theme.Pretendard
+import com.daejol.presentation.ui.theme.Red100
 import com.daejol.presentation.ui.theme.White100
-import com.daejol.presentation.ui.theme.secondaryLight
 
 @Composable
 fun PopularCatDogCard(
@@ -52,72 +56,104 @@ fun PopularCatDogCard(
             .wrapContentHeight()
             .width(width)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data("https://cdn2.thecatapi.com/images/cqg.jpg")
-                    .crossfade(true)
-                    .build(),
-                placeholder = painterResource(id = R.drawable.sample_cat),
-                contentDescription = stringResource(R.string.popular_catdog_image),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(dimensionResource(id = R.dimen.popular_catdog_image_height))
-            )
+        Box(modifier = Modifier.fillMaxWidth()) {
             Column(
                 modifier = Modifier
-                    .padding(
-                        horizontal = dimensionResource(id = R.dimen.space_xs),
-                        vertical = dimensionResource(id = R.dimen.space_xxs)
-                    )
+                    .fillMaxWidth()
             ) {
-                Text(
-                    text = "CAT NO.${catdog.ranking}",
-                    fontSize = dimensionResource(id = R.dimen.text_xxxs).value.sp,
-                    fontFamily = MoveSans,
-                    fontWeight = FontWeight.Bold,
-                    color = secondaryLight,
-                    lineHeight = 8.sp
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("https://cdn2.thecatapi.com/images/cqg.jpg")
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(id = R.drawable.sample_cat),
+                    contentDescription = stringResource(R.string.popular_catdog_image),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(dimensionResource(id = R.dimen.popular_catdog_image_height))
                 )
-                Text(
-                    text = catdog.name,
-                    fontSize = dimensionResource(id = R.dimen.text_xs).value.sp,
-                    fontFamily = MoveSans,
-                    fontWeight = FontWeight.Bold,
-                    lineHeight = 12.sp
-                )
-                Text(
-                    text = "고양이 귀여움 어쩌구 저쩌구...\napi temperament에 있는 설명..",
-                    fontSize = dimensionResource(id = R.dimen.text_xxxs).value.sp,
-                    fontFamily = MoveSans,
-                    fontWeight = FontWeight.Bold,
-                    lineHeight = 8.sp
-                )
-                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_xs)))
-                Row {
-                    Spacer(modifier = Modifier.weight(1f))
-                    IconButton(
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier.size(12.dp)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.FavoriteBorder,
-                            contentDescription = stringResource(R.string.popular_catdog_like)
+                Column(
+                    modifier = Modifier
+                        .padding(
+                            horizontal = dimensionResource(id = R.dimen.space_xs),
+                            vertical = dimensionResource(id = R.dimen.space_xxs)
                         )
+                ) {
+//                Text(
+//                    text = "CAT NO.${catdog.ranking}",
+//                    fontSize = dimensionResource(id = R.dimen.text_xxxs).value.sp,
+//                    fontFamily = MoveSans,
+//                    fontWeight = FontWeight.Bold,
+//                    color = secondaryLight,
+//                    lineHeight = 8.sp
+//                )
+                    Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.space_xxs)))
+                    Text(
+                        text = catdog.name,
+                        fontSize = dimensionResource(id = R.dimen.text_xs).value.sp,
+                        fontFamily = MoveSans,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 12.sp
+                    )
+                    Text(
+                        text = "고양이 귀여움 어쩌구 저쩌구...\napi temperament에 있는 설명..",
+                        fontSize = dimensionResource(id = R.dimen.text_xxxs).value.sp,
+                        fontFamily = MoveSans,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 8.sp
+                    )
+                    Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_xs)))
+                    Row {
+                        Spacer(modifier = Modifier.weight(1f))
+                        IconButton(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.size(12.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.FavoriteBorder,
+                                contentDescription = stringResource(R.string.popular_catdog_like)
+                            )
+                        }
                     }
                 }
             }
+            Ranking(catdog.ranking)
         }
+    }
+}
+
+@Composable
+private fun Ranking(
+    ranking: Int = 1
+) {
+    Box(
+        modifier = Modifier
+            .size(dimensionResource(id = R.dimen.popular_catdog_ranking_padding))
+            .padding(12.dp)
+    ) {
+        Text(
+            text = ranking.toString(),
+            fontSize = dimensionResource(id = R.dimen.text_xs).value.sp,
+            fontFamily = Pretendard,
+            fontWeight = FontWeight.SemiBold,
+            color = White100,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .size(dimensionResource(id = R.dimen.popular_catdog_ranking_size))
+                .drawBehind {
+                    drawCircle(
+                        color = Red100,
+                        radius = this.size.maxDimension / 2f
+                    )
+                }
+        )
     }
 }
 
 @Preview
 @Composable
-fun PopularCatDogCardPreView() {
+fun PopularCatDogCardPreview() {
     CatdogcupTheme {
         PopularCatDogCard()
     }
