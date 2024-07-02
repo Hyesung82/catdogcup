f95f4f7ae5393ef8823b928c50ba6cd394f70bbe
keykat <koin12000@naver.com>
2024-07-03 03:41:05 +0900

    [feature] 데이터 호출 방식 수정

--- domain/src/main/java/usecase/GetImageUsecase.kt
+++ domain/src/main/java/usecase/GetImageUsecase.kt
@@ -1,14 +1,56 @@
 package usecase
 
 import com.daejol.domain.repository.CatImagesRepository
+import com.daejol.domain.repository.DogImagesRepository
 import entity.ImageEntity
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
+enum class WorldCupType {
+    CAT, DOG, COMBINED
+}
+
 class GetImageUsecase @Inject constructor(
-    private val catImagesRepository: CatImagesRepository
+    private val catImagesRepository: CatImagesRepository,
+    private val dogImagesRepository: DogImagesRepository
 ) {
-    suspend fun getRandomCatImages(randomImageCount: Int): Flow<List<ImageEntity>?>? {
+    suspend fun getAnimalList(
+        type: WorldCupType,
+        randomImageCount: Int
+    ): Flow<List<ImageEntity>> {
+        return when (type) {
+            WorldCupType.CAT -> getRandomCatImages(randomImageCount)
+                ?: flow { listOf<ImageEntity>() }
+
+            WorldCupType.DOG -> getRandomCatImages(randomImageCount)
+                ?: flow { listOf<ImageEntity>() }
+
+            WorldCupType.COMBINED -> merge(
+                getRandomCatImages(randomImageCount / 2)?.map { it } ?: flow { },
+                getRandomDogImages(randomImageCount / 2)?.map { it } ?: flow { }
+            )
+        }
+    }
+
+    private suspend fun getRandomCatImages(randomImageCount: Int): Flow<List<ImageEntity>>? {
+        try {
+            val flow = catImagesRepository.getCatRandomImages(randomImageCount).map {
+                return@map it.on(
+                    // TODO: Entity 자체를 넘겨주는 것이 좋을까? 아니면 정말 필요한 것만 한 번 더 정제해서
+                    // TODO: 주는 것이 나을까?
+                    onSuccess = { it },
+                    // TODO: onError일 때 어떤 데이터를 던져주는 것이 좋을까?
+                    onError = { throw Exception() }
+                ) ?: listOf<ImageEntity>()
+            }
+
+            return flow
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
+    private suspend fun getRandomDogImages(randomImageCount: Int): Flow<List<ImageEntity>>? {
         try {
             val flow = catImagesRepository.getCatRandomImages(randomImageCount).map {
                 return@map it.on(
@@ -21,7 +63,7 @@ class GetImageUsecase @Inject constructor(
                     onError = {
                         throw Exception()
                     }
-                )
+                ) ?: listOf<ImageEntity>()
             }
 
             return flow
