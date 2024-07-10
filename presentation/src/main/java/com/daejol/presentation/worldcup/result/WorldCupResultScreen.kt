5d1c9c3c02104af66e4c8fd15dba40beb9f284f2
keykat <koin12000@naver.com>
2024-07-10 18:41:17 +0900

    [feature] 이동할 화면 추가

---
+++ presentation/src/main/java/com/daejol/presentation/worldcup/result/WorldCupResultScreen.kt
@@ -0,0 +1,13 @@
+package com.daejol.presentation.worldcup.result
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.daejol.presentation.worldcup.WorldCupViewModel
+
+@Composable
+fun WorldCupResultScreen(
+    viewModel: WorldCupViewModel,
+    navController: NavController? = null
+) {
+
+}
