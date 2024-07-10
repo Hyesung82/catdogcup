6da1c4e93737d1cced88ebb1ead619622451c4ba
Hyesung82 <khs990419@gmail.com>
2024-07-10 21:13:44 +0900

    [design] 홈 화면 여백 수정

--- presentation/src/main/java/com/daejol/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/daejol/presentation/home/HomeScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.daejol.presentation.R
 import com.daejol.presentation.ui.theme.CatdogcupTheme
@@ -72,7 +71,7 @@ fun HomeScreen(
         ) {
             item(span = StaggeredGridItemSpan.FullLine) {
                 Column {
-            WorldCupContent(navController)
+                    WorldCupContent(navController)
                     Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_m)))
                     Title(text = R.string.popular_catdog_title)
                     Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_xs)))
