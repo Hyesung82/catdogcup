7ddb2ecb9449c9ec2998604b8be0c3b9b502318e
Hyesung82 <khs990419@gmail.com>
2024-07-15 17:57:05 +0900

    [design] 인기 개냥이 상세화면 수정

--- presentation/src/main/java/com/daejol/presentation/home/PopularCatDogDetailScreen.kt
+++ presentation/src/main/java/com/daejol/presentation/home/PopularCatDogDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.daejol.presentation.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,15 +9,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -33,58 +38,74 @@ import com.daejol.presentation.ui.theme.Typography
 fun PopularCatDogDetailScreen(
     catDog: CatDog = TestData.catdogs[0]
 ) {
-    Scaffold(
-        topBar = {
-            PopularCatDogTopBar(catDog = catDog)
-        }
-    ) { innerPadding ->
+    Scaffold { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
         ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(TestData.catdogs[0].image)
-                    .crossfade(true)
-                    .build(),
-                placeholder = painterResource(id = R.drawable.sample_cat),
-                contentDescription = "인기 개냥이"
-            )
-            Column(
-                modifier = Modifier.padding(dimensionResource(id = R.dimen.space_s))
-            ) {
-                CatDogDetailAttribute(title = "Weight", desc = catDog.weight)
-                CatDogDetailAttribute(title = "Temperament", desc = catDog.temperament.toString())
-                CatDogDetailAttribute(title = "Origin", desc = catDog.origin)
-                CatDogDetailAttribute(title = "Description", desc = catDog.description)
-                CatDogDetailAttribute(title = "Life Span", desc = catDog.lifeSpan)
-                CatDogDetailAttribute(title = "Wikipedia URL", desc = catDog.wikipediaUrl)
-            }
+            CatDogDetailImage()
+            CatDogDetailDescription(catDog)
         }
+    }
+}
+
+@Composable
+private fun CatDogDetailImage() {
+    Box(modifier = Modifier) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(TestData.catdogs[0].image)
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(id = R.drawable.sample_cat),
+            contentDescription = "인기 개냥이"
+        )
+        PopularCatDogTopBar()
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun PopularCatDogTopBar(
-    catDog: CatDog
-) {
-    // TODO: CollapsingToolbarLayout 적용
+private fun PopularCatDogTopBar() {
     TopAppBar(
-        title = {
-            Row {
-                // TODO: 고양이 아이콘 & 강아지 아이콘
-//                Image(painter = , contentDescription = )
-                Text(text = catDog.name)
+        title = {},
+        navigationIcon = {
+            IconButton(onClick = { /* TODO: 뒤로 가기 */ }) {
+                Icon(imageVector = Icons.Outlined.ArrowBackIosNew, contentDescription = "뒤로 가기")
             }
         },
-        actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Outlined.Close, contentDescription = "뒤로 가기")
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent),
+        modifier = Modifier
+    )
+}
+
+@Composable
+private fun CatDogDetailDescription(
+    catDog: CatDog
+) {
+    Column(
+        modifier = Modifier.padding(dimensionResource(id = R.dimen.space_m))
+    ) {
+        Row {
+            Text(
+                text = catDog.name,
+                style = Typography.headlineLarge,
+                modifier = Modifier.weight(1f)
+                    .align(Alignment.CenterVertically)
+            )
+            IconButton(onClick = { /* TODO: 좋아요 */ }) {
+                Icon(imageVector = Icons.Outlined.FavoriteBorder, contentDescription = "좋아요")
             }
         }
-    )
+        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_l)))
+        CatDogDetailAttribute(title = "Weight", desc = catDog.weight)
+        CatDogDetailAttribute(title = "Temperament", desc = catDog.temperament.toString())
+        CatDogDetailAttribute(title = "Origin", desc = catDog.origin)
+        CatDogDetailAttribute(title = "Description", desc = catDog.description)
+        CatDogDetailAttribute(title = "Life Span", desc = catDog.lifeSpan)
+        CatDogDetailAttribute(title = "Wikipedia URL", desc = catDog.wikipediaUrl)
+    }
 }
 
 @Composable
@@ -93,7 +114,6 @@ private fun CatDogDetailAttribute(
     desc: String
 ) {
     Column {
-        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_l)))
         Text(
             text = title,
             style = Typography.titleLarge
@@ -102,6 +122,7 @@ private fun CatDogDetailAttribute(
             text = desc,
             style = Typography.bodyMedium
         )
+        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_l)))
     }
 }
 
