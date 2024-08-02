e0ba5c4a6b3b153b9cadd363b2322f67da1612ba
Hyesung82 <khs990419@gmail.com>
2024-08-02 18:14:22 +0900

    [refactor] companion object -> object 변경

--- data/src/main/java/com/daejol/data/ApiConst.kt
+++ data/src/main/java/com/daejol/data/ApiConst.kt
@@ -1,10 +1,8 @@
 package com.daejol.data
 
-class ApiConst {
-    companion object {
-        const val CAT_API_URL = "https://api.thecatapi.com/v1/"
-        const val DOG_API_URL = "https://api.thedogapi.com/v1/"
-        const val CAT_API_KEY = BuildConfig.CAT_API_KEY
-        const val DOG_API_KEY = BuildConfig.DOG_API_KEY
-    }
+object ApiConst {
+    const val CAT_API_URL = "https://api.thecatapi.com/v1/"
+    const val DOG_API_URL = "https://api.thedogapi.com/v1/"
+    const val CAT_API_KEY = BuildConfig.CAT_API_KEY
+    const val DOG_API_KEY = BuildConfig.DOG_API_KEY
 }
